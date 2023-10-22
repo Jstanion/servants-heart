d868c0cd935220955dcae17783d521b841d2e82f
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-22 13:23:30 -0500

    added sizing and font properties to second div

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
             Nourishing Lives, Empowering Communities
           </p>
           <div className="flex">
-            <p className="flex flex-col justify-around bg-white opacity-50 text-neutral p-6 mr-2 w-1/2 text-base">
+            <p className="flex flex-col justify-around bg-white opacity-50 text-neutral p-6 mr-2 w-1/2 text-lg">
               <span className="font-bold text-xl">Community:</span> We are committed to building a stronger, compassionate
               community.
               <br />
@@ -27,19 +27,14 @@ export default function Home() {
             <HeroCommitment />
           </div>
         </div>
-        <div className="p-4 mt-8 w-5/6 bg-secondary">
+        <div className="p-4 mt-8 w-5/6 h-2/3 bg-secondary">
           <div className="flex">
-            <p className="flex flex-col justify-around bg-white opacity-50 text-neutral p-6 mr-2 w-1/2 text-base">
-              <span className="font-bold text-xl"></span> We are committed to building a stronger, compassionate
-              community.
-              <br />
-              <span className="font-bold text-xl">Family:</span> We are committed to the well-being of every
-              family we serve.
-              <br />
-              <span className="font-bold text-xl">Faith:</span> We are committed to faith guiding our
-              values and inspiring others to join in our mission.
-            </p>
             <HeroMission />
+            <div className="grid bg-white opacity-50 p-6 ml-2 w-1/3">
+            <p className="self-center text-neutral text-lg text-center leading-loose">
+            A Servant&apos;s Heart Community Outreach is committed to serve the needs of the community while walking in love without judgement. It is our mission to feed both the hearts and stomachs of those within our community.
+            </p>
+            </div>
           </div>
         </div>
 
