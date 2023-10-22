6b1106eb5d1bf6d0503b14a818d214ad526c2174
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-22 13:33:25 -0500

    changed layout and color properties of third div

--- app/page.js
+++ app/page.js
@@ -15,31 +15,46 @@ export default function Home() {
           </p>
           <div className="flex">
             <p className="flex flex-col justify-around bg-white opacity-50 text-neutral p-6 mr-2 w-1/2 text-lg">
-              <span className="font-bold text-xl">Community:</span> We are committed to building a stronger, compassionate
-              community.
+              <span className="font-bold text-xl">Community:</span> We are
+              committed to building a stronger, compassionate community.
               <br />
-              <span className="font-bold text-xl">Family:</span> We are committed to the well-being of every
-              family we serve.
+              <span className="font-bold text-xl">Family:</span> We are
+              committed to the well-being of every family we serve.
               <br />
-              <span className="font-bold text-xl">Faith:</span> We are committed to faith guiding our
-              values and inspiring others to join in our mission.
+              <span className="font-bold text-xl">Faith:</span> We are committed
+              to faith guiding our values and inspiring others to join in our
+              mission.
             </p>
             <HeroCommitment />
           </div>
         </div>
+
         <div className="p-4 mt-8 w-5/6 h-2/3 bg-secondary">
           <div className="flex">
             <HeroMission />
             <div className="grid bg-white opacity-50 p-6 ml-2 w-1/3">
-            <p className="self-center text-neutral text-lg text-center leading-loose">
-            A Servant&apos;s Heart Community Outreach is committed to serve the needs of the community while walking in love without judgement. It is our mission to feed both the hearts and stomachs of those within our community.
-            </p>
+              <p className="self-center text-neutral text-lg text-center leading-loose">
+                A Servant&apos;s Heart Community Outreach is committed to serve
+                the needs of the community while walking in love without
+                judgement. It is our mission to feed both the hearts and
+                stomachs of those within our community.
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="">
-          <HeroStory />
+        <div className="p-4 my-6 w-5/6 h-2/3 bg-accent">
+          <div className="flex">
+            <div className="grid bg-white opacity-50 p-6 mr-2 w-1/3">
+              <p className="self-center text-neutral text-lg text-center leading-loose">
+                A Servant&apos;s Heart Community Outreach is committed to serve
+                the needs of the community while walking in love without
+                judgement. It is our mission to feed both the hearts and
+                stomachs of those within our community.
+              </p>
+            </div>
+            <HeroStory />
+          </div>
         </div>
       </div>
     </main>
