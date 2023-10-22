5420111e6cf3f50fd1d661e10126500ae84fa48e
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-21 23:22:06 -0500

    created sliding list component file for landing page

---
+++ app/component/slidingList/SlidingList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SlidingList = () => {
+  return (
+    <div>SlidingList</div>
+  )
+}
+
+export default SlidingList
--- app/page.js
+++ app/page.js
@@ -2,13 +2,15 @@ import React from "react";
 import HeroMission from "./component/hero/HeroMission";
 import HeroStory from "./component/hero/HeroStory";
 import HeroCommitment from "./component/hero/HeroCommitment";
+import SlidingList from "./component/slidingList/SlidingList";
 
 export default function Home() {
   return (
     <main>
-      <HeroCommitment />
+      <SlidingList />
+      {/* <HeroCommitment />
       <HeroMission />
-      <HeroStory />
+      <HeroStory /> */}
     </main>
   );
 }
