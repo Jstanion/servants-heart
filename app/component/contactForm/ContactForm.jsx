d84ac9a450be8358271068319ae66967e8ac08e9
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-26 15:29:28 -0500

    added dropdown selection tool

--- app/component/contactForm/ContactForm.jsx
+++ app/component/contactForm/ContactForm.jsx
@@ -2,16 +2,28 @@ import React from "react";
 
 const ContactForm = () => {
   return (
-    <div className="bg-white opacity-50 w-full">
-      <section>
-        <select className="select select-bordered select-sm w-full max-w-xs">
-          <option disabled selected>
-            Small
-          </option>
-          <option>Small Apple</option>
-          <option>Small Orange</option>
-          <option>Small Tomato</option>
+    <div className="flex justify-center w-full">
+      <section className="flex flex-col justify-center items-center w-4/5 h-full">
+        <div className="w-full">
+          <h1 className="text-4xl text-neutral text-start font-extralight">
+            CONTACT <span className="font-bold">US</span>
+          </h1>
+        </div>
+        <p className="my-8 text-sm">
+          Thank you for your interest. Our mission is to provide support to the community of Lancaster County. Let us know a little more about you, or feel free to browse our
+          website to learn more about what we do, and how you might be able to
+          join the outreach!
+        </p>
+        <select className="select select-bordered select-sm w-full text-neutral bg-white">
+          <option>Select one...</option>
+          <option>General Information</option>
+          <option>Project/Partnership</option>
+          <option>Volunteer</option>
+          <option>Donations</option>
         </select>
+        <p className="my-8 text-sm">
+        Not finding what you’re looking for on our website? Please complete the form below. We carefully review all submissions, although we may not be able to provide an immediate response. We kindly request your patience as we aim to personally address each one.
+        </p>
       </section>
     </div>
   );
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 const Contact = () => {
   return (
-    <div className="flex w-full my-4 bg-accent">
+    <div className="flex w-full my-4 p-4 bg-accent">
       <ContactForm />
-      <Image src="/donationImg.jpg" alt="Contact" width={500} height={500} className="p-4 w-fit h-fit"/>
+      <Image src="/donationImg.jpg" alt="Contact" width={500} height={500} className="w-fit h-fit pl-4"/>
     </div>
   )
 }
