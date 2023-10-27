a1ff322f00c9314b9694abc591a0e386de12a29d
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-27 15:03:32 -0500

    added nodemailer function and env variables

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -1,5 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
 
 export async function POST(NextRequest) {
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MY_EMAIL,
+      pass: process.env.MY_PASSWORD,
+    },
+  });
+
   return NextResponse.json('Hello from API!');
 }
