0b6e484e1a8087fff33e9ec83e8c0abb4a0b7411
Jstanion <123262263+Jstanion@users.noreply.github.com>
2024-04-25 18:54:13 -0500

    added page breaks to text and div containers

--- app/page.js
+++ app/page.js
@@ -7,24 +7,24 @@ import SlidingList from "./component/slidingList/SlidingList";
 export default function Home() {
   return (
     <main className="min-h-screen w-full">
-      <SlidingList />
-      <div className="pt-4 pr-4">
-        <div className="p-4 w-5/6 bg-info">
-          <h1 className="text-white p-4 mb-2 text-4xl text-center border-2 border-white border-opacity-50">
+      {/* <SlidingList /> */}
+      <div className="pt-4 lg:pr-4">
+        <div className="p-4 lg:w-5/6 bg-info">
+          <h1 className="text-white lg:p-4 mb-2 lg:text-4xl text-center border-2 border-white border-opacity-50">
             <span className="font-extralight">Nourishing </span>
             <span className="font-bold">Lives, </span>
             <span className="font-extralight">Empowering </span>
             <span className="font-bold">Communities</span>
           </h1>
-          <div className="flex">
-            <p className="flex flex-col justify-around bg-white opacity-50 text-neutral p-6 mr-2 w-1/2 text-lg">
-              <span className="font-bold text-xl">Family:</span> We are
+          <div className="flex flex-col-reverse lg:flex-row">
+            <p className="flex flex-col justify-around bg-white opacity-50 text-neutral p-6 lg:mr-2 lg:w-1/2 lg:text-lg">
+              <span className="font-bold text-xl lg:text-5xl">Family</span> We are
               committed to the well-being of every family we serve.
               <br />
-              <span className="font-bold text-xl">Community:</span> We are
+              <span className="font-bold text-xl lg:text-5xl">Community</span> We are
               committed to building a stronger, compassionate community.
               <br />
-              <span className="font-bold text-xl">Faith:</span> We are committed
+              <span className="font-bold text-xl lg:text-5xl">Faith</span> We are committed
               to faith guiding our values and inspiring others to join in our
               mission.
             </p>
