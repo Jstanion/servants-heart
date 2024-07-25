eced5eee022360a0bc52b13d08ff917b3a548ee2
Jstanion <123262263+Jstanion@users.noreply.github.com>
2024-07-24 22:45:47 -0500

    moved main container out of slider component

--- app/component/slidingList/SlidingList.jsx
+++ app/component/slidingList/SlidingList.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 const SlidingList = () => {
   return (
-    <div className="sticky top-0 bottom-0">
-      <div className="absolute bg-primary right-0 w-1/6 my-4">
+    
         <div className="bg-primary m-2 lg:m-4">
           <ul className="bg-white opacity-50 text-neutral text-center font-bold list-disc list-inside lg:p-2">
-            <span className="text-lg lg:text-xl">Current Needs</span>
-            <li className="font-normal text-start pl-2 lg:pl-4">Diapers</li>
-            <li className="font-normal text-start pl-2 lg:pl-4">Clothing</li>
-            <li className="font-normal text-start pl-2 lg:pl-4">Toiletries</li>
-            <li className="font-normal text-start pl-2 lg:pl-4">Toys</li>
-            <li className="font-normal text-start pl-2 lg:pl-4">Bicycles</li>
-            <li className="font-normal text-start pl-2 lg:pl-4">Non-Perishables</li>
-            <li className="font-normal text-start pl-2 lg:pl-4">Feminine Products</li>
-            <li className="font-normal text-start pl-2 lg:pl-4">Blankets</li>
+            <span className="text-3xl">Current Needs</span>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Diapers</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Clothing</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Toiletries</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Toys</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Bicycles</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Non-Perishables</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Gift Cards</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Blankets</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">Furniture</li>
+            <li className="font-normal text-2xl text-start pl-2 lg:pl-4">School Supplies</li>
           </ul>
         </div>
-      </div>
-    </div>
+      
   );
 };
 
