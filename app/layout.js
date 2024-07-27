814a72886e619aac51ca75af530d2301924981b4
Jstanion <123262263+Jstanion@users.noreply.github.com>
2024-07-26 21:38:52 -0500

    added metadata for safari browser users

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./component/navbar/Navbar";
 import Footer from "./component/footer/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <meta name={metadata.title} content={metadata.description} />
+        <title>{metadata.title}</title>
+      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
