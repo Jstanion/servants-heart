cbea1eafb00af43952d62c7a78df67e42bc69cf6
Jstanion <123262263+Jstanion@users.noreply.github.com>
2024-07-31 21:13:46 -0500

    added Safari webkit

--- app/component/hero/HeroCommitment.jsx
+++ app/component/hero/HeroCommitment.jsx
@@ -3,21 +3,22 @@ import React from "react";
 const HeroCommitment = () => {
   return (
     <div className="hero h-fit">
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="object-cover"
-        >
-          <source src="/heroCommit.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      <div className="hero-overlay bg-opacity-60">
-      </div>
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="video-element object-cover"
+      >
+        <source src="/heroCommit.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-info">
         <div className="max-w-md">
-          <h1 className="mb-5 text-4xl lg:text-5xl font-bold">Our Commitment</h1>
+          <h1 className="mb-5 text-4xl lg:text-5xl font-bold">
+            Our Commitment
+          </h1>
         </div>
       </div>
     </div>
